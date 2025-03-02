7ec99c04f6a57c3b845707eb0ae428d079fc7c94
abbdr <142697206+abbdr@users.noreply.github.com>
2025-03-02 22:46:08 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -13,11 +13,9 @@ post = pd.read_csv('dataset_tweet_sentiment_pilkada_DKI_2017.csv')
 #### Menggunakan Algoritma K-Nearest Neighbor
 #### Dihitung menggunakan Cosine Similarity, salah satu metode untuk mengetahui kemiripan antarkalimat
 #### Setiap kalimat diberikan pembobotan dengan menggunakan metode TF IDF (Terms Frequeny - Inverse Document Frequency)
-#
 '''
 
 post
 
-'#'
 
 st.page_link('pages/1-preprocessing.py', label='preprocessing')
