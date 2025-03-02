32e7488909ce820947fce8bd8558d699e2247052
Rozag <www.rozag@gmail.com>
2025-03-02 23:20:55 +0700

    update

--- test.py
+++ test.py
@@ -10,18 +10,19 @@ X_train, y_train, X_test, y_test = train_test_split(df)
 
 knn = KNN.KNN()
 knn.fit(X_train, y_train)
-predictions = knn.predict(X_test)
+ready_test, predictions = knn.predict(X_test)
 
 def calculate_accuracy(predictions):
   prediction = []
-  if(len(predictions.keys())==2):
-    predictions = 'positive' if predictions['positive'] > predictions['negative'] else 'negative'
-    prediction.append(predictions)
-  elif(len(predictions.keys())==1):
-    predictions = list(predictions.keys())[0]
-    prediction.append(predictions)
-  else:
-    prediction.append(np.nan)
+  for i in predictions:
+    if(len(i.keys())==2):
+      i = 'positive' if i['positive'] > i['negative'] else 'negative'
+      prediction.append(i)
+    elif(len(i.keys())==1):
+      i = list(i.keys())[0]
+      prediction.append(i)
+    else:
+      prediction.append(np.nan)
 
   acc = []
   for pred, real in zip(prediction, y_test):
